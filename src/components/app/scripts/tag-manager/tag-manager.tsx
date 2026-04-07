6617ef3eb5c621cc81756d4c091c8df56d9ab306
Anastas Vartanyan <vaawebdev@gmail.com>
2026-04-07 15:15:14 +0400

    testid removed

--- src/components/app/scripts/tag-manager/tag-manager.tsx
+++ src/components/app/scripts/tag-manager/tag-manager.tsx
@@ -4,15 +4,12 @@ import { type FC } from 'react'
 export const TagManager: FC = () => {
   return process.env.NEXT_PUBLIC_GTM_ID ? (
     <>
-      <Script
-        id="gtm"
-        data-testid="gtm-script"
-      >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script id="gtm">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-${process.env.NEXT_PUBLIC_GTM_ID}');`}</Script>
-      <Script id="gtm-gtag" data-testid="gtm-script-consent">{`
+      <Script id="gtm-gtag">{`
         window.dataLayer = window.dataLayer || [];
 
         window.gtag = function gtag() {
